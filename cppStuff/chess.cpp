d28ebf7e124865a3278a5272ff31939e6a817c68
Hassam Sulehria <sulehria.hassam@gmail.com>
2021-05-18 02:00:33 -0400

    Fixed printBoard() function to allow for it to actually print the board

--- cppStuff/chess.cpp
+++ cppStuff/chess.cpp
@@ -6,7 +6,7 @@ Board::Board(){
 
     //Initialize white pieces
     pieceBB[wP] =    0x000000000000FF00;
-    pieceBB[wK] =    0x0000000000000042;
+    pieceBB[wN] =    0x0000000000000042;
     pieceBB[wR] =    0x0000000000000081;
     pieceBB[wB] =    0x0000000000000024;
     pieceBB[wQ] =    0x0000000000000010;
@@ -14,7 +14,7 @@ Board::Board(){
 
     //Initialize black pieces
     pieceBB[bP] =    0x00FF000000000000;
-    pieceBB[bK] =    0x4200000000000000;
+    pieceBB[bN] =    0x4200000000000000;
     pieceBB[bR] =    0x8100000000000000;
     pieceBB[bB] =    0x2400000000000000;
     pieceBB[bQ] =    0x1000000000000000;
--- cppStuff/main.cpp
+++ cppStuff/main.cpp
@@ -1,43 +1,91 @@
 #include <iostream>
 #include "chess.h"
 
-int* getBoardFromBB(U64 bitBoard[12]);
+void getBoardFromBB(U64 bitBoard[12], int board[64]);
 void printBoard(int board[64]);
 
 
 int main(){
     Board board;
-    printBoard(getBoardFromBB(board.getPieceBB()));
+    int scalarBoard[64] = {0};
+    getBoardFromBB(board.getPieceBB(), scalarBoard);
+    printBoard(scalarBoard);
     
     return 0;
 }
 
 //Gets the board so that it can be easily printed, maybe replace with precreated board and just use BB for move gen but eh
-int* getBoardFromBB(U64 bitBoard[14]){
-    int board[64] = {0};
+void getBoardFromBB(U64 bitBoard[14], int board[64]){
+    //int board[64] = {0};
     U64 bitTracker = 0x0000000000000001; //Can shift to get bits in BB
     for (int i = 0; i < 12; i++){
+        bitTracker = 0x0000000000000001;
         for (int j = 0; j < 64; j++){
-            if (bitTracker && bitBoard[i] == 0) board[j] = i + 1;
+            if ((bitTracker & bitBoard[i]) == bitTracker) board[j] = i + 1;
             bitTracker = bitTracker << 1;
         }
     }
 
 
-    return board;
+    //return board;
 }
 
+//Helper function for printBoard which turns a number into a piece
+char numberToPiece(int index){
+    switch(index) {
+        case 1:
+            return 'p';
+            break;
+        case 2:
+            return 'n';
+            break;
+        case 3:
+            return 'b';
+            break;
+        case 4:
+            return 'r';
+            break;
+        case 5:
+            return 'q';
+            break;
+        case 6:
+            return 'k';
+            break;
+        case 7:
+            return 'P';
+            break;
+        case 8:
+            return 'N';
+            break;
+        case 9:
+            return 'B';
+            break;
+        case 10:
+            return 'R';
+            break;
+        case 11:
+            return 'Q';
+            break;
+        case 12:
+            return 'K';
+            break;
+        default:
+            return '0';
+    }
+}
+
+//Prints the board
 void printBoard(int board[64]){
     int counter = 0;
-    std::cout << "a b c d e f g h" << std::endl;
     for (int row = 0; row < 8; row++){
-        std::cout << row << " ";
+        std::cout << row + 1 << "  ";
         for (int col = 0; col < 8; col++){
-            std::cout << board[counter] << " ";
+            std::cout <<numberToPiece(board[counter]) << " ";
             counter++;
         } 
         std::cout << "\n";
     }
     //delete [] board;
+    std::cout << "   a b c d e f g h" << std::endl;
 
 }
