8b824222683773a91e828175df7921e4cf4bd402
Hassam Sulehria <sulehria.hassam@gmail.com>
2021-05-20 00:57:44 -0400

    Changed board representation from an 8x8 board to a 10x12 for easier and cleaner move generation

--- pythonStuff/chess.py
+++ pythonStuff/chess.py
@@ -1,80 +1,11 @@
 #A class for pieces
 
 class Board:
-    def __init__(self, fenString = 0):
-        if fenString != 0:
-            self.board = fen_to_board(fenString)
+    def __init__(self, fenString = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"):
         #Turns a FEN string into a board
+        #Originally a helper function, might become one again to make code cleaner or for easily switching states
         #Currently a 8x8 representation, might alter to 10x12 to make easier
-        
-            board = []
-            empty = -1  #Maybe change to empty = 0, invalid = -1
-            p = 0
-            n = 1
-            r = 2
-            b = 3
-            q = 4
-            k = 5
-            P = 6
-            N = 7
-            R = 8
-            B = 9
-            Q = 10
-            K = 11
-            bl = 1
-            wh = -1
-           #Note: it is kinda pointless to differentiate between p and P 
-            for letter in fenString:
-                #12 pieces could use better approach but lazy
-                if letter == "p":
-                    board.append(Square(p, bl))
-                elif letter == "P":
-                    board.append(Square(P, wh))
-                elif letter == "b":
-                    board.append(Square(b, bl))
-                elif letter == "B":
-                    board.append(Square(B, wh))
-                elif letter == "n":
-                    board.append(Square(n, bl))
-                elif letter == "N":
-                    board.append(Square(N, wh))
-                elif letter == "Q":
-                    board.append(Square(Q, wh))
-                elif letter == "q":
-                    board.append(Square(q, bl))
-                elif letter == "r":
-                    board.append(Square(r, bl))
-                elif letter == "R":
-                    board.append(Square(R, wh))
-                elif letter == "k":
-                    board.append(Square(k,bl))
-                elif letter == "K":
-                    board.append(Square(K,wh))
-                #Exit for now here because I don't want to implement/fix
-                elif letter == " ":
-                    break
-                #Spaces
-                elif letter != "/":
-                    for i in range(int(letter)):
-                        board.append(Square(empty,0)) #Let this represent an empty square for now
-
-                self.board = board
     
-        else:
-            self.board = [Square(0, 0)]*120 #10x12 array
-            
-    def getBoardState(self):
-        return self.board
-
-    def updateState(self, state):
-        self.board = state
-
-
-
-#print(fen_to_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))
-    #Turns a FEN string into a board
-    #Currently a 8x8 representation, might alter to 10x12 to make easier
-    """def fen_to_board(fenString):
         board = []
         empty = -1  #Maybe change to empty = 0, invalid = -1
         p = 0
@@ -125,16 +56,24 @@ class Board:
             elif letter != "/":
                 for i in range(int(letter)):
                     board.append(Square(empty,0)) #Let this represent an empty square for now
+             
+        self.board = convertTo10x12(board) #lol i'm a big clown, spent an hour debugging looking turns out it was one extra indent...
 
-        return board"""   
+            
+    def getBoardState(self):
+        return self.board
 
-    #Move a piece from one position to another
+    def updateState(self, state):
+        self.board = state
+
     def movePiece(self, pos1, pos2):
         #Maybe have a special rule here for castling
         self.board[place2] = self.board[place1]
         self.board[place1] = 0
         return board
-  
+    
+    #MOVE GENERATION FUNCTIONS
+    #------------------------------------------------------------------------#
     #Get moves functions
     def getAllValidMoves(colour):
         pass
@@ -144,64 +83,50 @@ class Board:
        pass
 
 
-#print(fen_to_board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))
+    #Pawns can move +-8 positions on board when unnocupied
+    #Pawns can capture enemy pieces that are +- 7 or 9
+    #INSERT EN POSSANT RULES
+    def getPawnMoves(self, board, position, color):
+        possibleMoves = []
+        
+        #Variable definitions to make code more readable
+        empty = 0
+        move1 = 10*color + position  #8*color + position  (8x8 Versions)
+        move2 = 20*color + position #16*color + position
+        moveW = 9*color + position #7*color + position
+        moveE = 11*color + position #9*color + position
 
-#Turns a FEN string into a board
-#Currently a 8x8 representation, might alter to 10x12 to make easier
-def fen_to_board(fenString):
-    board = []
-    empty = -1  #Maybe change to empty = 0, invalid = -1
-    p = 0
-    n = 1
-    r = 2
-    b = 3
-    q = 4
-    k = 5
-    P = 6
-    N = 7
-    R = 8
-    B = 9
-    Q = 10
-    K = 11
-    bl = 1
-    wh = -1
-   #Note: it is kinda pointless to differentiate between p and P 
-    for letter in fenString:
-        #12 pieces could use better approach but lazy
-        if letter == "p":
-            board.append(Square(p, bl))
-        elif letter == "P":
-            board.append(Square(P, wh))
-        elif letter == "b":
-            board.append(Square(b, bl))
-        elif letter == "B":
-            board.append(Square(B, wh))
-        elif letter == "n":
-            board.append(Square(n, bl))
-        elif letter == "N":
-            board.append(Square(N, wh))
-        elif letter == "Q":
-            board.append(Square(Q, wh))
-        elif letter == "q":
-            board.append(Square(q, bl))
-        elif letter == "r":
-            board.append(Square(r, bl))
-        elif letter == "R":
-            board.append(Square(R, wh))
-        elif letter == "k":
-            board.append(Square(k,bl))
-        elif letter == "K":
-            board.append(Square(K,wh))
-        #Exit for now here because I don't want to implement/fix
-        elif letter == " ":
-            break
-        #Spaces
-        elif letter != "/":
-            for i in range(int(letter)):
-                board.append(Square(empty,0)) #Let this represent an empty square for now
+        #Normal move 1 square, technically don't have to check if in bounds because it will transform anyways
+        if (board[move1].colour == empty):
+            possibleMoves.append(move1)
+            #Possibility to move 2 squares when at starting position, maybe just have an unmoved flag to keep code nicer
+            if (position > 30 and position < 39) or (position > 80 and position < 89): 
+                if (board[move2].colour == empty): 
+                     possibleMoves.append(move2)
+        #Capture enemy piece (West) aka shift 7 
+        if (board[moveW].colour == -color): #shouldnt seg fault anymore 
+            possibleMoves.append(moveW)
+        #Capture enemy piece (East) aka shift 9
+        if (board[moveE].colour == -color): #eh I'll fix the segfault bug later after I fully decide how board representation will work
+            possibleMoves.append(moveE)
 
-    return board
+        #Insert en possant code
+            
 
+        return possibleMoves
+
+    def getKnightMoves(self, board, position, color):
+        pass
+
+#Helper function that converts the 8x8 array to a 10x12 array
+#Because I'm too lazy to edit the fenstring code
+def convertTo10x12(board):
+    invalid = -2
+    newBoard = [Square(invalid, invalid)]*120
+    for row in range(2, 10):
+        for col in range(1, 9):
+            newBoard[row*10 + col] =  board[(row-2)*8+(col-1)]         
+    return newBoard
 class Square:
     #Let black = 1; white = -1 and empty square has colour of 0
     def __init__(self, piece, colour):
@@ -210,42 +135,16 @@ class Square:
         self.colour = colour
         #self.image = 0
     
-
-    #Pawns can move +-8 positions on board when unnocupied
-    #Pawns can capture enemy pieces that are +- 7 or 9
-    #INSERT EN POSSANT RULES
-    def getPawnMoves(self, board, position, color):
-        possibleMoves = []
-        #Normal move 1 square, technically don't have to check if in bounds because it will transform anyways
-        if (board[8*color + position].colour == 0):
-            possibleMoves.append(8*color + position)
-
-            #Possibility to move 2 squares when at starting position
-            if (position >= 8 and position < 16) or (position < 56 and position >= 48) and (position + 16*colour < 64 and position + 16*colour >= 0):
-                if (board[16*color + position].colour == 0):
-                     possibleMoves.append(16*color + position)
-        #Capture enemy piece (West) aka shift 7 
-        if (position % 8 != 0 and board[7*color + position].colour == -color): #eh I'll fix the segfault bug later after I fully decide how board representation will work
-            possibleMoves.append(7*color + position)
-        #Capture enemy piece (East) aka shift 9
-        if (position + 1 % 8 != 0 and board[9*color + position].colour == -color): #eh I'll fix the segfault bug later after I fully decide how board representation will work
-            possibleMoves.append(9*color + position)
-
-        #Insert en possant code
-            
-
-        return possibleMoves
-
-    def getTypeOfMoves(board, position):
-        if board[position] == 1:
-            getPawnMoves(board, position, "black")
-        elif board[position] == 9:
-            getPawnMoves(board, position, "white")
                                                   
  
 fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-chessB = Board(fen)
-qBert = chessB.board
-pawnMv = (chessB.board[8]).getPawnMoves(chessB.board, 9, 1)
+chessB = Board()
+pawnMv = chessB.getPawnMoves(chessB.board, 83, chessB.board[83].colour)
+"""for i in range(12):
+    word = ""
+    for j in range(10):
+        word += str(chessB.board[i*10 + j].colour)
+    print(word)"""
+
 print(pawnMv)
 
